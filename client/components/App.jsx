38c82b4331a0789604e22bc32c536fc8e6c01a1a
krjw-00 <kriswd0@gmail.com>
2021-06-28 07:00:47 +1200

    tweaks some files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,12 +11,25 @@ function App (props) {
   return (
     <>
       <div className='app'>
-        <h1 className="title">Fullstack Boilerplate - with Fruits!</h1>
+        <section className="hero is-primary is-small">
+          <div className="hero-body">
+            <h1 className="title is-1">Gold Digger</h1>
+            <p className="subtitle is-3">Gold tracker for the kids</p>
+          </div>
+        </section>
         <ul>
           {props.fruits.map(fruit => (
             <li key={fruit}>{fruit}</li>
           ))}
         </ul>
+        <div className="columns is-mobile is-centered is-primary">
+          <div className="column box is-primary">
+            <p className="title">Lily</p>
+            <p>Bla bla Bla</p>
+          </div>
+          <div className="column title">Fred</div>
+          <div className="column title">Ruby</div>
+        </div>
       </div>
     </>
   )
